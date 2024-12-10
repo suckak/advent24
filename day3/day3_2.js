4f6eaead5b5a5aa0467e62c026fe2eeda81c468d
Daniel Gutierrez <cdanielgl@gmail.com>
2024-12-09 18:11:48 -0600

    bug on regex

--- day3/day3_2.js
+++ day3/day3_2.js
@@ -2,7 +2,8 @@ const fs = require('node:fs');
 
 try {
   let data = fs.readFileSync('./input.txt', 'utf8');
-
+  //.replace(/don't\(\).*?do\(\)/g, '');
+  // ITERATIVE
   while (data.includes("don't()")) {
     const start = data.indexOf("don't()");
     const end = data.indexOf('do()', start);
@@ -13,6 +14,7 @@ try {
     data = aux;
   }
 
+  fs.writeFileSync('./iterative.txt', data);
   const operations = data
     .split('mul')
     .filter((val) => val.startsWith('('))
@@ -21,6 +23,8 @@ try {
     )
     .filter((nums) => nums.length === 2 && !nums.includes(NaN))
     .filter((nums) => !nums.some((element) => element < 1 || element > 999));
+
+  console.log('ops', operations.length);
 
   const result = operations.reduce(
     (acc, current) => acc + current[0] * current[1],
