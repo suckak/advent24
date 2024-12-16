5feeafb635925bb8dac4f6b7de7efee5b1497399
Daniel Gutierrez <cdanielgl@gmail.com>
2024-12-16 10:17:27 -0600

    refactor day10 part two

--- day10/day10_2.js
+++ day10/day10_2.js
@@ -20,8 +20,7 @@ try {
     .split(/\r?\n|\r|\n/g)
     .map((val) => val.split('').map(Number));
 
-  console.log(matrix);
-  const getNextStep = (coordinate, matrix, omit) => {
+  const getNextStep = (coordinate, matrix) => {
     const xLimit = matrix.length - 1;
     const yLimit = matrix[0].length - 1;
     const paths = [];
@@ -47,45 +46,27 @@ try {
       paths.push(new Coordinate(coordinate.x - 1, coordinate.y, matrix));
     }
 
-    // omit.forEach((element) => {
-    //   const deleteIndex = paths.findIndex((val) => val.id === element.id);
-
-    //   if (deleteIndex !== -1) {
-    //     paths.splice(deleteIndex);
-    //   }
-    // });
-
     return paths.filter((val) => {
       return curr + 1 === matrix[val.x][val.y];
     });
   };
 
-  let top = [];
   const total = [];
 
-  const checkPath = (coord, path, used, matrix) => {
-    // console.log(coord);
-    if (
-      matrix[coord.x][coord.y] === 9 &&
-      !used.includes((val) => val.id === coord.id)
-    ) {
-      top.push([...path.flat(), coord].flat());
-      return [[...path.flat(), coord].flat()];
+  const checkPath = (coord, path, matrix) => {
+    if (matrix[coord.x][coord.y] === 9) {
+      const result = [...path.flat(), coord].flat();
+      return [result];
     }
 
-    const nextSteps = getNextStep(coord, matrix, used);
+    const nextSteps = getNextStep(coord, matrix);
     if (nextSteps.length < 1) {
-      return;
+      return [];
     }
 
-    return nextSteps.map((val) =>
-      checkPath(
-        val,
-        [...path.flat(), coord].flat(),
-        [...used.flat(), coord].flat(),
-        matrix
-      )
-    );
+    return nextSteps
+      .map((val) => checkPath(val, [...path, coord], matrix))
+      .flat();
   };
 
   for (let x = 0; x < matrix.length; x++) {
@@ -93,20 +74,8 @@ try {
     for (let y = 0; y < line.length; y++) {
       const element = line[y];
       if (element === 0) {
-        top = [];
-        const test = checkPath(
-          new Coordinate(x, y, matrix),
-          [],
-          [],
-          matrix,
-          []
-        );
-        const unique = new Map();
-        top.forEach((arr) => {
-          const title = arr.reduce((acc, current) => acc + current.id, '');
-          unique.set(title, arr);
-        });
-        total.push(unique.size);
+        const test = checkPath(new Coordinate(x, y, matrix), [], matrix);
+        total.push(test.length);
       }
     }
   }
@@ -115,20 +84,6 @@ try {
     'total',
     total.reduce((acc, current) => acc + current, 0)
   );
-  // console.log(checkPath(createCoord(0, 2), [], [], matrix));
-  // const coord = new Coordinate(6, 6, matrix);
-  // console.log(coord);
-  // // console.log(getNextStep(coord, matrix, []));
-  // const test = checkPath(coord, [], [], matrix, []);
-  // const unique = new Map();
-  // top.forEach((arr) => {
-  //   const title = arr.reduce((acc, current) => acc + current.id, '');
-  //   unique.set(arr[arr.length - 1].id, arr);
-  // });
-  // console.log('map', unique);
-  // console.log('check', test);
-  // console.log('top', top);
-  // console.log('aaaa');
 } catch (err) {
   console.error(err);
 }
